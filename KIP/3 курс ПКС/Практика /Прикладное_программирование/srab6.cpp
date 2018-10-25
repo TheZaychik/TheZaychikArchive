8114e892212386119c13984e8d7754e355800b36
Nikita Zaytsev <nikitos945max@gmail.com>
2018-10-25 12:04:13 +0300

    upd_2_0_5_5

---
+++ KIP/3 курс ПКС/Практика /Прикладное_программирование/srab6.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// вар 10 ПП9
+class Human {
+public:
+    string name, family, patron;
+public:
+    Human(string _name, string _family, string _patron) {
+        this->name = _name;
+        this->family = _family;
+        this->patron = _patron;
+    }
+
+    void show();
+};
+
+class Buhgalter : Human{
+private:
+    int oklad;
+public:
+    Buhgalter(string _name, string _family, string _patron, int _oklad) : Human(_name, _family, _patron){
+        this->oklad = _oklad;
+    }
+
+    void show(){
+        cout << "Obj: " << name << " " << family << " " << patron << endl;
+        cout << "Buhgalter with oklad " << oklad << endl;
+    }
+};
+
+class Svarshik : Human{
+private:
+    int razryad;
+public:
+    Svarshik(string _name, string _family, string _patron, int _razryad) : Human(_name, _family, _patron){
+        this->razryad = _razryad;
+    }
+    void show(){
+        cout << "Obj: " << name << " " << family << " " << patron << endl;
+        cout << "Svarshik with razryad " << razryad << endl;
+    }
+};
+
+int main(){
+    Buhgalter b("Ivan", "Ivanov", "Ivanovich", 200);
+    Svarshik s("Anatoly","Sergeev","Sergeevich", 1);
+    b.show();
+    s.show();
+    return 0;
+}
