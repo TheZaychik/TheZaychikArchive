5bb5b2bacce7107d7e6a5a72ab4b8e69f70bacf1
Nikita Zaytsev <nikitos945max@gmail.com>
2018-11-28 11:01:42 +0300

    upd_2_0_6_4_1

--- KIP/3 курс ПКС/Практика /PriklProg/para18.cpp
+++ KIP/3 курс ПКС/Практика /PriklProg/para18.cpp
@@ -1,4 +1,16 @@
-//
-// Created by Nikita Zaytsev on 28/11/2018.
-//
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    FILE *f = fopen("textfiles/s1.txt", "w+");
+    fprintf(f, "%d", a);
+    fscanf(f, "%d", &a);
+    fclose(f);
+    f = fopen("textfiles/s2.txt", "w+");;
+    a += 3;
+    fprintf(f, "%d", a);
+    fclose(f);
+}
